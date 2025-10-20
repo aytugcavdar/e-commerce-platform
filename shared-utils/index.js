5c2c334be1a9900a59cf5c6c55534733d9870f35
aytug cavdar <aytugcavdar@gmail.com>
2025-10-20 03:24:43 +0300

    Add shared utilities module exports for logger, middleware, helpers, constants, validators, and RabbitMQ

---
+++ shared-utils/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  logger: require('./logger'),
+  middleware: require('./middleware'),
+  helpers: require('./helpers'),
+  constants: require('./constants'),
+  validators: require('./validators'), 
+  rabbitmq: require('./rabbitmq'),   
+};
